94a925e84e59ea3b0a0262a43da0105c637e1cb3
AgustinMansur <agustinmansur@gmail.com>
2022-12-03 11:41:32 -0300

    sumando sidebar y boton(con errores

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,30 @@
+import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/navbar/navbar.jsx';
+import Button from './components/button';
+import Sidebar from './components/sidebar';
+import Header from './components/header';
+import { PRODUCTS } from './constants/data/products';
+import Card from './components/card';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [showProducts, setShowProducts] = useState(false);
+
+  const onHandlerCart = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar>
-        </Navbar>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Sidebar onClose={onHandlerCart} isOpen={isOpen} />
+      <Header numbersOfItems={0} onHandlerCart={onHandlerCart} />
+      <h1>Productos destacados</h1>
+        <div className='products-container'>
+        {PRODUCTS.map((product) => (
+          <Card product={product} key={product.name} />
+        ))}
+      </div>
     </div>
   );
 }
--- my-app/src/components/navbar/index.jsx
+++ my-app/src/components/navbar/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './styles.css';
-import Button from "../button/button";
+import Button from "../button";
 
 
 const Navbar = ({}) => {
@@ -8,11 +8,10 @@ const Navbar = ({}) => {
         <div className="navbar-menu col-12">
                 <h2 className="navbar-logo col-md-3 col.sm-2">🐱‍🚀</h2>
                 <div className="navbar-item col-md-9 col-sm-10">
-                <Button text={"boton1"}></Button>
-                <Button text={"boton2"}></Button>
-                <Button text={"boton3"}></Button>
-                <Button text={"boton4"}></Button>
-                <Button text={"boton5"}></Button>
+                <Button text={"categoria 1"}></Button>
+                <Button text={"categoria 2"}></Button>
+                <Button text={"categoria 3"}></Button>
+                <Button text={"categoria 4"}></Button>
                 </div>
         </div>
     )
